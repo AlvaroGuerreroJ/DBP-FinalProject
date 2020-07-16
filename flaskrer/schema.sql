681ffa13d3b555d97f5e0189d25ae1c22255ae76
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2020-07-16 16:27:54 -0500

    feat: Implement database
    - Define schema
    - Add command to initialize database

---
+++ flaskrer/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS image_post;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE image_post (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  hash TEXT NOT NULL,
+  original_filename TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  alternative_text TEXT NOT NULL,
+  description TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
