4f8d5cc4d5b34a27b5e4877c78a1fd3886652272
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2020-07-19 18:56:44 -0500

    feat: Implement /create

--- flaskrer/__init__.py
+++ flaskrer/__init__.py
@@ -8,7 +8,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskrer.sqlite')
+        DATABASE=os.path.join(app.instance_path, 'flaskrer.sqlite'),
+        UPLOAD_FOLDER=os.path.join(app.instance_path, 'Pics')
     )
 
     if test_config is None:
--- flaskrer/pics.py
+++ flaskrer/pics.py
@@ -13,3 +13,126 @@ from flaskrer.auth import login_required
 from flaskrer.db import get_db
 
 bp = Blueprint('pics', __name__)
+
+allowed_file_extensions = {'png', 'jpg', 'jpeg'}
+
+
+class FlaskrerError(Exception):
+    pass
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        alternative_text = request.form['alternative_text']
+        description = request.form['description']
+        if not description:
+            description = ''
+        picture = request.files['picture']
+
+        try:
+            if not picture or picture.filename == '':
+                raise FlaskrerError('No picture selected for upload')
+            if not title:
+                raise FlaskrerError('Title required')
+            if not alternative_text:
+                raise FlaskrerError('Alternative text required')
+
+            original_filename = picture.filename
+            if not allowed_file(original_filename):
+                raise FlaskrerError('File extension not allowed')
+
+            # XXX: There may be a way to hash the file without saving it first
+            _, temp_filename = tempfile.mkstemp()
+
+            picture.save(temp_filename)
+
+            picture_extension = imghdr.what(temp_filename)
+            if picture_extension is None or \
+               picture_extension not in allowed_file_extensions:
+                raise FlaskrerError(
+                    'File is not of one of the supported types'
+                )
+
+            picture_hash = generate_hash(temp_filename)
+
+            # Check that the file has not been uploaded before
+            db = get_db()
+            if db.execute(
+                    'SELECT * FROM image_post'
+                    ' WHERE author_id = ? AND hash = ?',
+                    (session.get('user_id'), picture_hash)
+            ).fetchone() is not None:
+                raise FlaskrerError(
+                    f'You have already uploaded {original_filename}.'
+                )
+
+        except FlaskrerError as e:
+            flash(str(e))
+            return render_template('pics/create.html')
+
+        user_pictures_directory = get_user_upload_directory(g.user['username'])
+        try:
+            os.makedirs(user_pictures_directory)
+        except OSError:
+            pass
+
+        picture_store_location = os.path.join(
+            user_pictures_directory,
+            f'{picture_hash}.{picture_extension}'
+        )
+        shutil.move(temp_filename, picture_store_location)
+
+        db = get_db()
+        db.execute(
+            'INSERT INTO image_post'
+            ' (author_id, hash, original_filename, title, alternative_text,'
+            ' description)'
+            ' VALUES (?, ?, ?, ?, ?, ?)',
+            (session['user_id'], picture_hash, original_filename, title,
+             alternative_text, description)
+        )
+        db.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('pics/create.html')
+
+
+# XXX: This is the same as in auth.py. There may be a way to refactore it.
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?',
+            (user_id,)
+        ).fetchone()
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in allowed_file_extensions
+
+
+def generate_hash(filename):
+    h = hashlib.sha256()
+
+    with open(filename, 'rb') as fp:
+        while True:
+            chunk = fp.read(h.block_size)
+            if not chunk:
+                break
+            h.update(chunk)
+
+    return h.hexdigest()
+
+
+def get_user_upload_directory(username):
+    return os.path.join(current_app.config['UPLOAD_FOLDER'],
+                        username)
